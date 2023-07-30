cb9798aecc60b5a7b7b94c9c0681be7e4f9f09c5
paul-njuabeh <117046940+paul-njuabeh@users.noreply.github.com>
2023-07-29 23:26:24 -0700

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,7 @@
 def call(Map stageParams) {
- 
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.main ]],
-        userRemoteConfigs: [[ url: stageParams.https://github.com/paul-njuabeh/CI-CD-Project ]]
+        userRemoteConfigs: [[ url: 'https://github.com/paul-njuabeh/CI-CD-Project' ]]
     ])
   }
